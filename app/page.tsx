68e78fa2f0ce968da3ea418ea5e9a89da1e420bc
Astha <asthajain497@gmail.com>
2025-01-02 12:49:36 +0530

    all sections final

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 const Page = () => {
   const [activeTab, setActiveTab] = useState("technical");
@@ -23,21 +23,16 @@ const Page = () => {
 
   // Update progress values when the active tab changes
   useEffect(() => {
-    // Reset progress to 0% when changing tabs
     setProgressValues(new Array(4).fill(0));
 
-    // Determine which skills to show based on the active tab
     const targetSkills = activeTab === "technical" ? technicalSkills : softSkills;
     const newProgressValues = targetSkills.map((skill) => skill.progress);
 
-    // Add a small delay before updating the progress values to trigger animation
     setTimeout(() => {
       setProgressValues(newProgressValues);
     }, 100); // 100ms delay to ensure the progress starts at 0
   }, [activeTab]);
 
-
-  
   return (
     <>
       {/* Hero Section */}
@@ -46,29 +41,13 @@ const Page = () => {
           <h1 className="display-1">Astha Jain</h1>
           <p className="lead">Passionate about changing the world with technology.</p>
           <div className="social-links">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Mohd-shahid-iqbal"
-              aria-label="My GitHub"
-            >
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Mohd-shahid-iqbal" aria-label="My GitHub">
               <i className="fab fa-github fa-3x socialicons"></i>
             </a>
-            
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.instagram.com"
-              aria-label="My Instagram"
-            >
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com" aria-label="My Instagram">
               <i className="fab fa-instagram fa-3x socialicons"></i>
             </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com"
-              aria-label="My LinkedIn"
-            >
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com" aria-label="My LinkedIn">
               <i className="fab fa-linkedin fa-3x socialicons"></i>
             </a>
           </div>
@@ -81,26 +60,16 @@ const Page = () => {
       {/* About Me Section */}
       <section id="aboutme">
         <div className="about-container">
-          <img
-            src="/images/project1.jpeg" // Ensure this path is correct
-            alt="Profile"
-            className="about-image"
-          />
+          <img src="/images/project1.jpeg" alt="Profile" className="about-image" />
           <div className="about-text">
             <h2>About Me</h2>
             <p>
-              My name is Astha Jain. I am a graduate of 2024 from Teerthanker Mahaveer University at Moradabad with a degree in B.Tech CSE. 
-              I am most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. 
+              My name is Astha Jain. I am a graduate of 2024 from Teerthanker Mahaveer University at Moradabad with a degree in B.Tech CSE.
+              I am most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering.
               In my free time I like working on open source projects.
             </p>
             {/* Resume Button */}
-            <a
-              href="/resume.pdf" // Ensure the correct path to your resume file
-              download
-              className="resume-btn"
-              role="button"
-              aria-label="Download Resume"
-            >
+            <a href="/resume.pdf" download className="resume-btn" role="button" aria-label="Download Resume">
               Download Resume
             </a>
           </div>
@@ -114,59 +83,39 @@ const Page = () => {
           {/* First Row (Two Companies) */}
           <div className="experience-row">
             <div className="experience-item">
-              <img
-                src="/images/2C.png" // Ensure this path is correct
-                alt="Teerthanker Mahaveer University"
-                className="company-logo"
-              />
+              <img src="/images/2C.png" alt="Teerthanker Mahaveer University" className="company-logo" />
               <p><strong>Role:</strong> Social Media Manager</p>
               <p><strong>Duration:</strong> July 2023 – December 2024</p>
             </div>
-
             <div className="experience-item">
-              <img
-                src="/images/2C.png" // Ensure this path is correct
-                alt="Teerthanker Mahaveer University"
-                className="company-logo"
-              />
-             <p><strong>Role:</strong> Web Developer Intern</p>
-             <p><strong>Duration:</strong> June 2023– August 2023</p>
-            </div>
-            
-
-          {/* Second Row (Third Company) */}
-          <div className="experience-row">
-          <div className="experience-item">
-              <img
-                src="/images/3C.jpeg" // Ensure this path is correct
-                alt="Firstcry.com"
-                className="company-logo"
-              />
-              <p><strong>Role:</strong> Manager</p>
-              <p><strong>Duration:</strong> Fulltime</p>
+              <img src="/images/2C.png" alt="Teerthanker Mahaveer University" className="company-logo" />
+              <p><strong>Role:</strong> Web Developer Intern</p>
+              <p><strong>Duration:</strong> June 2023– August 2023</p>
             </div>
           </div>
 
+          {/* Second Row (Third Company) */}
+          <div className="experience-row">
             <div className="experience-item">
-              <img
-                src="/images/1C.jpeg" // Ensure this path is correct
-                alt="Allsoft Solutions"
-                className="company-logo"
-              />
-               <p><strong>Role:</strong> Web Developer Intern</p>
-               <p><strong>Duration:</strong> June 2022 – August 2022</p>
+              <img src="/images/3C.jpeg" alt="Firstcry.com" className="company-logo" />
+              <p><strong>Role:</strong> Manager</p>
+              <p><strong>Duration:</strong> Fulltime</p>
+            </div>
+            <div className="experience-item">
+              <img src="/images/1C.jpeg" alt="Allsoft Solutions" className="company-logo" />
+              <p><strong>Role:</strong> Web Developer Intern</p>
+              <p><strong>Duration:</strong> June 2022 – August 2022</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section id="projects">
-  <h2>Recent Projects</h2>
-
-  <div className="projects-container">
-    {/* MyPortfolio Project */}
+<section id="projects" className="projects-section">
   
-
+    <h2 >Recent Projects</h2>
+  
+  <div className="projects-container">
+    {/* First Project Card */}
     <div className="project-card">
       <h3 className="project-title">MyPortfolio</h3>
       <div className="project-links">
@@ -218,17 +167,18 @@ const Page = () => {
       </div>
 
       <div className="project-links">
-  <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers" 
-  target="_blank" 
-  className="btn-small">
-    <span className="text-dark card-link mr-4">
-      <i className="fab fa-github"></i> Stars 
-    </span>
-  </a>
-  <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
-</div>
+        <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers"
+          target="_blank"
+          className="btn-small">
+          <span className="text-dark card-link mr-4">
+            <i className="fab fa-github"></i> Stars
+          </span>
+        </a>
+        <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
+      </div>
     </div>
 
+    {/* Second Project Card */}
     <div className="project-card">
       <h3 className="project-title">MyPortfolio</h3>
       <div className="project-links">
@@ -280,17 +230,18 @@ const Page = () => {
       </div>
 
       <div className="project-links">
-  <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers" 
-  target="_blank" 
-  className="btn-small">
-    <span className="text-dark card-link mr-4">
-      <i className="fab fa-github"></i> Stars 
-    </span>
-  </a>
-  <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
-</div>
+        <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers"
+          target="_blank"
+          className="btn-small">
+          <span className="text-dark card-link mr-4">
+            <i className="fab fa-github"></i> Stars
+          </span>
+        </a>
+        <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
+      </div>
     </div>
 
+    {/* Third Project Card */}
     <div className="project-card">
       <h3 className="project-title">MyPortfolio</h3>
       <div className="project-links">
@@ -342,17 +293,18 @@ const Page = () => {
       </div>
 
       <div className="project-links">
-  <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers" 
-  target="_blank" 
-  className="btn-small">
-    <span className="text-dark card-link mr-4">
-      <i className="fab fa-github"></i> Stars 
-    </span>
-  </a>
-  <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
-</div>
+        <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers"
+          target="_blank"
+          className="btn-small">
+          <span className="text-dark card-link mr-4">
+            <i className="fab fa-github"></i> Stars
+          </span>
+        </a>
+        <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
+      </div>
     </div>
 
+    {/* Fourth Project Card */}
     <div className="project-card">
       <h3 className="project-title">MyPortfolio</h3>
       <div className="project-links">
@@ -404,107 +356,92 @@ const Page = () => {
       </div>
 
       <div className="project-links">
-  <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers" 
-  target="_blank" 
-  className="btn-small">
-    <span className="text-dark card-link mr-4">
-      <i className="fab fa-github"></i> Stars 
-    </span>
-  </a>
-  <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
-</div>
-    </div>
-  </div>
-</section>
-
-{/* Leadership Section */}
-<section id="leadership">
-  <div className="container py-5">
-    <h2 className="display-4 pb-5 text-center">Leadership</h2>
-    <div className="row justify-content-center"> {/* Added justify-content-center to center the content */}
-      <div className="col-md-8"> {/* Set width to 8 columns to ensure the text is properly aligned */}
-        <p className="lead text-center"> {/* Added text-center to center the paragraph text */}
-          As a React developer with leadership experience, I bring technical expertise, project management skills, and strong communication abilities to the table. I have a proven track record of building large-scale applications, optimizing code for performance, and implementing best practices. Additionally, I have experience leading teams and projects, creating and maintaining project plans, managing timelines and budgets, and ensuring timely delivery of high-quality work. I excel at communicating effectively with team members, stakeholders, and clients, both verbally and in writing, and am a skilled listener who can provide clear and constructive feedback. I am also passionate about mentoring and training other developers to help them develop their skills and achieve their goals. With my ability to solve complex technical problems and think creatively, I am always up-to-date with the latest developments in React and related technologies, and am able to innovate and inspire a positive, collaborative work environment.
-        </p>
+        <a href="https://github.com/Mohd-shahid-iqbal/myportfolio/stargazers"
+          target="_blank"
+          className="btn-small">
+          <span className="text-dark card-link mr-4">
+            <i className="fab fa-github"></i> Stars
+          </span>
+        </a>
+        <span className="project-update"><strong>Updated on :</strong>January 17, 2024</span>
       </div>
     </div>
   </div>
 </section>
 
 
-     
+      {/* Leadership Section */}
+      <section id="leadership">
+        <div className="container py-5">
+          <h2 className="display-4 pb-5 text-center">Leadership</h2>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <p className="lead text-center">
+                As a React developer with leadership experience, I bring technical expertise, project management skills, and strong communication abilities to the table. I have a proven track record of building large-scale applications, optimizing code for performance, and implementing best practices. Additionally, I have experience leading teams and projects, creating and maintaining project plans, managing timelines and budgets, and ensuring timely delivery of high-quality work. I excel at communicating effectively with team members, stakeholders, and clients, both verbally and in writing, and am a skilled listener who can provide clear and constructive feedback. I am also passionate about mentoring and training other developers to help them develop their skills and achieve their goals. With my ability to solve complex technical problems and think creatively, I am always up-to-date with the latest developments in React and related technologies, and am able to innovate and inspire a positive, collaborative work environment.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
+      {/* Skills Section */}
       <section id="skills" className="skills">
-      <h2>Skills</h2>
-      <div className="tabs">
-        <button
-          className={`tabButton ${activeTab === "technical" ? "activeTab" : ""}`}
-          onClick={() => setActiveTab("technical")}
-        >
-          Technical Skills
-        </button>
-        <button
-          className={`tabButton ${activeTab === "soft" ? "activeTab" : ""}`}
-          onClick={() => setActiveTab("soft")}
-        >
-          Soft Skills
-        </button>
-      </div>
-      <div className="tabContent">
-        {activeTab === "technical" && (
-          <ul className="skillList">
-            {technicalSkills.map((skill, index) => (
-              <li key={index} className="skillItem">
-                <span className="skillName">{skill.name}</span>
-                <div className="progressBar">
-                  <div
-                    className="progress"
-                    style={{ width: `${progressValues[index]}%` }}
-                  ></div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-        {activeTab === "soft" && (
-          <ul className="skillList">
-            {softSkills.map((skill, index) => (
-              <li key={index} className="skillItem">
-                <span className="skillName">{skill.name}</span>
-                <div className="progressBar">
-                  <div
-                    className="progress"
-                    style={{ width: `${progressValues[index]}%` }}
-                  ></div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </section>
+        <h2>Skills</h2>
+        <div className="tabs">
+          <button className={`tabButton ${activeTab === "technical" ? "activeTab" : ""}`} onClick={() => setActiveTab("technical")}>
+            Technical Skills
+          </button>
+          <button className={`tabButton ${activeTab === "soft" ? "activeTab" : ""}`} onClick={() => setActiveTab("soft")}>
+            Soft Skills
+          </button>
+        </div>
+        <div className="tabContent">
+          {activeTab === "technical" && (
+            <ul className="skillList">
+              {technicalSkills.map((skill, index) => (
+                <li key={index} className="skillItem">
+                  <span className="skillName">{skill.name}</span>
+                  <div className="progressBar">
+                    <div className="progress" style={{ width: `${progressValues[index]}%` }}></div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          {activeTab === "soft" && (
+            <ul className="skillList">
+              {softSkills.map((skill, index) => (
+                <li key={index} className="skillItem">
+                  <span className="skillName">{skill.name}</span>
+                  <div className="progressBar">
+                    <div className="progress" style={{ width: `${progressValues[index]}%` }}></div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </section>
 
-    <footer>
-  <div className="contact-info">
-    <h3>Get In Touch</h3>
-    <p>I am currently looking for full-time React Developer/Frontend Developer
-       opportunities! If you know of any positions available, if you have any 
-       questions, or if you just want to say hi, please feel free to email me at,  
-       <a href="asthajain497@gmail.com"> asthajain497@gmail.com</a>.</p>
-  </div>
-  <div className="social-links">
-    <a href="https://github.com/asthajain08" target="_blank" aria-label="GitHub">
-      <i className="fab fa-github"></i>
-    </a>
-    <a href="www.linkedin.com/in/astha-jain-881399201" target="_blank" aria-label="LinkedIn">
-      <i className="fab fa-linkedin"></i>
-    </a>
-  </div>
-</footer>
+      {/* Footer Section */}
+      <footer>
+        <div className="contact-info">
+          <h3>Get In Touch</h3>
+          <p>I am currently looking for full-time React Developer/Frontend Developer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at, <a href="mailto:asthajain497@gmail.com">asthajain497@gmail.com</a>.</p>
+        </div>
+        <div className="social-links">
+          <a href="https://github.com/asthajain08" target="_blank" aria-label="GitHub">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/astha-jain-881399201" target="_blank" aria-label="LinkedIn">
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
+      </footer>
 
-<button className="scroll-to-top" onClick="scrollToTop()">↑</button>
-
+      <button className="scroll-to-top" onClick="scrollToTop()">↑</button>
     </>
   );
-}
+};
+
 export default Page;
